708d06cace565d7f36b9657bec98c51858cdb14b
Abhijeet Nirphal <nirphalabhijeet72@gmail.com>
2024-03-17 14:10:37 +0530

    build: Add commit message check
    * Developer should not able commit
    * If commit conventions not followed

--- src/main/java/org/dnyanyog/config/RouterConfig.java
+++ src/main/java/org/dnyanyog/config/RouterConfig.java
@@ -8,11 +8,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RouterConfig {
 
-	@Bean
-	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route("auth_public_route", r -> r.path("/api/v1/public/auth/**").uri("http://localhost:8082"))
-				.route("auth_route", r -> r.path("/api/v1/auth/**").uri("http://localhost:8082"))
-				.route("customer_route", r -> r.path("/api/v1/customer/**").uri("http://localhost:8081")).build();
-	}
+  @Bean
+  public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+    return builder
+        .routes()
+        .route(
+            "auth_public_route", r -> r.path("/api/v1/public/auth/**").uri("http://localhost:8082"))
+        .route("auth_route", r -> r.path("/api/v1/auth/**").uri("http://localhost:8082"))
+        .route("customer_route", r -> r.path("/api/v1/customer/**").uri("http://localhost:8081"))
+        .build();
+  }
 }
